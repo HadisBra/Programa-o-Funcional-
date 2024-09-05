45986618f5e0e3996ed2876f4fd39b7fc773454e
Guilherme Alexandre Pereira Lima <113989944+HadisBra@users.noreply.github.com>
2024-09-05 10:28:47 -0300

    Add files via upload
    Leitura de Retorno de Boletos Bancários usando Strategy Pattern com Programação Funcional

---
+++ retorno-boleto-funcional/src/main/java/Principal.java
@@ -0,0 +1,21 @@
+import com.manoelcampos.retornoboleto.ProcessarBoletos;
+
+import java.net.URI;
+import java.net.URISyntaxException;
+
+/**
+ * Executa a aplicação para ler um arquivo de
+ * retorno de boleto e imprimir no terminal.
+ * @author manoelcampos
+ */
+public class Principal {
+    public static void main(String[] args) throws URISyntaxException {
+        var processador = new ProcessarBoletos(ProcessarBoletos::lerBancoBrasil);
+        URI caminhoArquivo = Principal.class.getResource("banco-brasil-1.csv").toURI();
+        processador.processar(caminhoArquivo);
+
+        caminhoArquivo = Principal.class.getResource("bradesco-1.csv").toURI();
+        processador = new ProcessarBoletos(ProcessarBoletos::lerBradesco);
+        processador.processar(caminhoArquivo);
+    }
+}
